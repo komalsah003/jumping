870fc2d68267b5dea5c8e98885f4552b951a3291
komal kumari <78696212+komalsah003@users.noreply.github.com>
2021-05-12 10:46:31 +0530

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -11,14 +11,14 @@ function Circle(x, y, dx, dy, radius) {
     this.dx = dx;
     this.dy = dy;
     this.radius = radius;
-
     this.draw = function () {
         c.beginPath();
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         c.strokeStyle = 'red';
         c.stroke();
+        c.fillStyle = this.color;
         c.fill();
-//        c.fillStyle="brown";
+        // c.fillStyle="";
     }
     this.update = function () {
         if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
@@ -30,24 +30,24 @@ function Circle(x, y, dx, dy, radius) {
         }
         this.x += this.dx;
         this.y += this.dy;
+
         this.draw();
     }
 }
 
 var circleArray = [];
-for (var i = 0; i <150; i++) {
-    var radius = 30;
+for (var i = 0; i <450; i++) {
+    var radius = 20;
     var x = Math.random() * (innerWidth-radius*2)+radius;
     var y = Math.random() * (innerHeight-radius*2)+radius;
     var dx = (Math.random() - 0.5);
     var dy = (Math.random() - 0.5)*8;
     circleArray.push(new Circle(x,y,dx,dy,radius));
-
 }
 
 function animate() {
     requestAnimationFrame(animate);
-//     c.clearRect(0, 0, innerWidth, innerHeight);
+    // c.clearRect(0, 0, innerWidth, innerHeight);
 for(var i=0;i<circleArray.length;i++){
     circleArray[i].update();
 }
